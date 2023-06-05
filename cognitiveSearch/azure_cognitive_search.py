5402e34693e30ccfc200f3ad5562b9a7b397b326
huqianghui <huqianghui0613@163.com>
2023-06-05 13:47:11 +0800

    Update azure_cognitive_search.py

--- cognitiveSearch/azure_cognitive_search.py
+++ cognitiveSearch/azure_cognitive_search.py
@@ -3,6 +3,6 @@ from cognitiveSearch.azureCognitiveSearchSemanticRetriever import AzureCognitive
 
 os.environ["AZURE_COGNITIVE_SEARCH_SERVICE_NAME"] = "gptkb-odk2jh5qispeo"
 os.environ["AZURE_COGNITIVE_SEARCH_INDEX_NAME"] ="dataingestionindex"
-os.environ["AZURE_COGNITIVE_SEARCH_API_KEY"] = "yjiXaGvg0VjX3JLkr0MHxpx20X0w4a1M8LtQnQsQ1EAzSeB9SOUw"
+os.environ["AZURE_COGNITIVE_SEARCH_API_KEY"] = "XXXXX"
 
 retriever = AzureCognitiveSearchSemanticRetriever(content_key="content")
